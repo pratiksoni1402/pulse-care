45004084f4dd0f01258b7de6cdde6edb31868d21
Pratik <qualdev.pratiksoni@gmail.com>
2026-07-06 16:11:32 +0530

    configured mysql

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,10 @@
-import { betterAuth } from 'better-auth'
-
+import { betterAuth, socialProviders } from 'better-auth'
+import { drizzleAdapter } from '@better-auth/drizzle-adapter'
+import { db } from '@/db'
 export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: 'mysql',
+  }),
   emailAndPassword: {
     enabled: true,
     requireEmailVerification: true,
